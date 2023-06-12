3544ed39098bd007897801705436264aae8759a5
amal-krishna-m-u <amalkrishna@ug.cusat.ac.in>
2023-06-12 16:47:13 +0000

    formating basics using fmt and printf

---
+++ formatprint.go
@@ -0,0 +1,26 @@
+
+//This program is to get to know about format printing : fmt 
+
+
+package main
+import "fmt"
+
+
+func main(){
+
+const passage, date, age = "This program is for educational purpose ","12-06-2023", 22
+
+fmt.Printf("%s and made on %s ,my age is %d ",passage ,date,age)
+
+/*
+fmt is a standard lib and Printf is a function that is used to print with formating 
+
+more details is available in the official website of golang 
+*/
+
+
+
+
+}
+
+
