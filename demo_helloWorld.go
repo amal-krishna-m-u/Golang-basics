5bf9c8b261fab30e36b917479240a45d88d4cacf
amalkrsihna <amalkrsihna@ug.cusat.ac.in>
2023-06-21 17:56:42 +0530

    renamed demo files

---
+++ demo_helloWorld.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+// This program uses standard library fmt for printing and formating :
+
+func main() {
+	fmt.Println("hello world")
+
+	hello()
+
+}
+func hello() {
+	fmt.Println("hello world form helloworld package")
+
+}
