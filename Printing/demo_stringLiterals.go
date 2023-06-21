813e764f1049383b12f81d9e4eec7131d52b5bd4
amalkrsihna <amalkrsihna@ug.cusat.ac.in>
2023-06-21 18:09:45 +0530

    printing using backticks

---
+++ Printing/demo_stringLiterals.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("you can use UTF-8 characters like emojies in golang 😊")
+	fmt.Println(`You can also use backticks to print multiple lines
+like this
+and this
+and this
+so on.
+This is how you print multiple lines in golang using backticks .And this will print the lines as it is written in the code`)
+	fmt.Println("you can also use double quotes to print multiple lines \n like this \n and this \n and this \n so on. \n This is how you print multiple lines in golang using double quotes .And this will print the lines as it is written in the code")
+
+}
